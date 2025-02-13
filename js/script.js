ac054b44e6a882006d4248a3466aac84bcae9588
ImDeech <rafaeldevinnn@gmail.com>
2025-02-13 07:49:40 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+    let messages = document.querySelectorAll(".message");
+    let currentIndex = 0;
     let envelope = document.querySelector(".envelope");
     let card = document.querySelector(".card");
+
+    function showNextMessage() {
+        messages[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % messages.length;
+        messages[currentIndex].classList.add("active");
+    }
+
+    setInterval(showNextMessage, 3000);
 
     envelope.addEventListener("click", function () {
         envelope.classList.add("opened"); // Tambahkan class untuk mengubah warna amplop
